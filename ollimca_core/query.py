e5ecde135f3d467f175a26fda38ed18c7b1dbc40
sammy fischer <samuel.fischer@smartinsurtech.de>
2024-12-23 13:40:19 +0100

    Improve search query formatting and start the event listener automatically instead of waiting for the sendbutton.

--- ollimca_core/query.py
+++ ollimca_core/query.py
@@ -36,11 +36,11 @@ class Query:
     def query_chroma(self, content, mood, colors, page, items_per_page):
         search_query = ""
         if content.strip() != "":
-            search_query += "description:" + content
+            search_query += "description:\"\"" + content+"\"\""
         if mood.strip() != "":
-            search_query += "\nmood: " + mood
+            search_query += "\nmood:\"\"" + mood+"\"\""
         if colors.strip() != "":
-            search_query += "\noverall_color_scheme: " + colors
+            search_query += "\noverall_color_scheme:\"\"" + colors+"\"\""
 
         response = ollama.embeddings(
             prompt=search_query,
